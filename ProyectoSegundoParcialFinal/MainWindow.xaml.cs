6886c0fb5504d34f59905672eaaa684abcb2f416
DamianBurboa <damianburboa1997@gmail.com>
2019-10-14 00:24:34 -0700

    Add project files.

---
+++ ProyectoSegundoParcialFinal/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoSegundoParcialFinal
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : UserControl
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CbProducto_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            grdProducto.Children.Clear();
+        }
+
+        private void Btnagregar_Click(object sender, RoutedEventArgs e)
+        {
+            switch (cbProducto.SelectedIndex)
+            {
+                case 0:
+                    grdProducto.Children.Add(new Anillo());
+                    break;
+                case 1:
+                    grdProducto.Children.Add(new Camiseta());
+                    break;
+                case 2:
+                    grdProducto.Children.Add(new Traje());
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
